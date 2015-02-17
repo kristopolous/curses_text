5283a5bbdd296b3d40b5817c7707dc764bc275a6
Chris McKenzie <kristopolous@yahoo.com>
2015-02-17 15:36:59 -0800

    implementation complete, now time to compile

--- ctext.cpp
+++ ctext.cpp
@@ -108,6 +108,7 @@ int8_t get_size(size_t*x, size_t*y)
 {
   *x = this->m_max_x;
   *y = this->m_max_y;
+
   return 0;
 }
 
@@ -181,7 +182,6 @@ int8_t ctext::rebuf()
   // issue a rescroll on exactly our previous parameters. This may
   // force us inward or may retain our position.
   return this->direct_scroll(this->m_pos_x, this->m_pos_y);
-  return 0;
 }
 
 int8_t ctext::printf(const char*format, ...)
@@ -217,12 +217,94 @@ int8_t ctext::render()
 
   if(!this->m_win)
   {
-    // not doing anything without a window.
+    // Not doing anything without a window.
     return -1;
   }
 
-  int32_t width = 0, height = 0;
-  getmaxyx(this->m_win, height, width);
+  this->get_win_size();
 
+  //
   // By this time, if we are bounded by a box,
+  // it has been accounted for.
+  //
+  // Really our only point of interest is
+  // whether we need to append to bottom
+  // or append to top.
+  //
+  // We will assume that we can
+  // populate the window quick enough
+  // to avoid linear updating or paging.
+  //  ... it's 2015 after all.
+  //
+  werase(this->m_win);
+
+  // Regardless of whether this is append to top
+  // or bottom we generate top to bottom.
+
+  size_t start_char = max(0, this->m_pos_y);
+  size_t offset = start_char;
+  // the endchar will be in the substr
+  
+  // We start as m_pos_x in our list and move up to
+  // m_pos_x + m_win_height except in the case of 
+  // wrap around.  Because of this special case,
+  // we compute when to exit slightly differently.
+  //
+  // This is the current line of output, which stays
+  // below m_win_height
+  size_t line = 0;
+
+  // start at the beginning of the buffer.
+  size_t index = this->m_pos_x;
+  size_t directionality = +1;
+
+  // if we are appending to the top then we start
+  // at the end and change our directionality.
+  if(this->m_config.m_append_top)
+  {
+    directionality = -1;
+    index = this->m_pos_x + this->m_win_height;
+  }
+
+  while(line < this->m_win_height)
+  {
+    // Reset the offset.
+    offset = start_char;
+
+    index += directionality;
+    if(index < this->m_max_y || index >= 0)
+    {
+      // We only index into the object if we have the
+      // data to do so.
+      wstring to_add = this->m_buffer[index].substr(start_char, this->m_win_width);
+      mvwaddwstr(this->m_win, line, 0, to_add);
+
+      // if we are wrapping, then we do that here.
+      while(
+          this->m_config.m_do_wrap && 
+
+          // if our string still exhausts our entire width
+          to_add.size() == this->m_win_width &&
+
+          // and we haven't hit the bottom
+          line < this->m_win_height
+        )
+      {
+        // move our line forward
+        line++;
+
+        // and the start_char
+        start_char += this->m_win_width;
+
+        // substring into this character now at this advanced position
+        to_add = this->m_buffer[index].substr(start_char, this->m_win_width);
+        
+        // and add it to the screen
+        mvwaddwstr(this->m_win, line, 0, to_add);
+      }
+    }
+    line++;
+  }
+
+  wrefresh(this->m_win);
 }
